ffc3e75ff39164b26ecd54e9abecad26571c6deb
HuanXinyue <lengleng0815@gmail.com>
2017-04-05 20:59:46 +0800

    Build a Random Quote Machine
    尚未实现分享api和点击动态切换谚语的功能

---
+++ Basic Front End Development Projects/scripts/quote.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Leng on 2017/4/5.
+ */
+
+(function() {
+    var btn = document.getElementById("new");
+    btn.onclick = function () {
+        $.getJSON("../data/quote.json",function(val) {
+            var i = 0;
+        })
+    }
+})();
